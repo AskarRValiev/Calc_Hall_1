15164d812bc980ea748aff1977350cb884a21014
Leonid <leonkor64@mail.ru>
2022-07-29 02:49:01 +0300

    add guid

---
+++ guid.py
@@ -0,0 +1,8 @@
+import complex_div as div
+import complex_mult as mul
+import complex_sub as sub
+import complex_sum as sum
+
+
+dict_ar = {'/': div, '*': mul, '-': sub, '+': sum}
+dict_log = {'/': 'деление', '*': 'умножение', '-': 'вычитание', '+': 'сложение'}
